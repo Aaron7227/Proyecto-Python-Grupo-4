ae2820fb92f90976bbcaa59bfb8c2e9197dd830b
EmilianoRamirez <eramirez60488@ufide.ac.cr>
2023-03-09 21:39:43 -0600

    registrar una cuenta de usuario

---
+++ parte_uno.py
@@ -0,0 +1,14 @@
+# Pedir información al usuario
+correoElectronico = input("Introduzca su correo electrónico: ")
+nombreComercio = input("Introduzca el nombre de su comercio: ")
+telefonoComercio = input("Introduzca el teléfono de su comercio: ")
+nombreDueno = input("Introduzca su nombre completo: ")
+ubicacionLocal = input("Introduzca la ubicación de su local: ")
+
+# Imprimir información registrada
+print("¡Registro exitoso!")
+print("Correo electrónico: ",correoElectronico)
+print("Nombre del comercio: ",nombreComercio)
+print("Teléfono del comercio: ",telefonoComercio)
+print("Nombre del dueño: ",nombreDueno)
+print("Ubicación del local: ",ubicacionLocal)
