f2784a8085d9d8e5943cb88f40c51d1f29422ddf
Aaron Arguedas <aaronau2003@gmail.com>
2023-03-10 10:10:46 -0600

    Añadido factura electrónica parte 1

--- parte_uno.py
+++ parte_uno.py
@@ -12,3 +12,26 @@ print("Nombre del comercio: ",nombreComercio)
 print("Teléfono del comercio: ",telefonoComercio)
 print("Nombre del dueño: ",nombreDueno)
 print("Ubicación del local: ",ubicacionLocal)
+
+#Factura electrónica
+
+#Preguntar por el tipo de cédula
+
+print("Digite que tipo de documento de identidad posee")
+print("(1) Para residente nacional")
+print("(2) Para residente extranjero-nacionalizado")
+print("(3) Cédula Jurídica \n")
+
+tipoCedula = int(input("Ingrese la opción: "))
+
+if tipoCedula == 1:
+    print("Residente nacional")
+    cedulaComprador=input("Digíte su número de cédula: \n")
+
+if tipoCedula == 2:
+    print("Residente extranjero nacionalizado")
+    cedulaComprador=input("Digíte su número de cédula: \n")
+
+if tipoCedula == 3:
+    print("Cédula juridica")
+    cedulaComprador=input("Digíte su número de cédula: \n")
