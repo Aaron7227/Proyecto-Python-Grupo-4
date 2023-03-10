c890cd70091ba59283f2f6733d9b94fbc1c809ec
Aaron Arguedas <aaronau2003@gmail.com>
2023-03-10 11:29:18 -0600

    Cambio nombre variables

--- parte_uno.py
+++ parte_uno.py
@@ -1,5 +1,5 @@
 # Pedir información al usuario
-correoElectronico = input("Introduzca su correo electrónico: ")
+correoElectronicoComercio = input("Introduzca su correo electrónico: ")
 nombreComercio = input("Introduzca el nombre de su comercio: ")
 telefonoComercio = input("Introduzca el teléfono de su comercio: ")
 nombreDueno = input("Introduzca su nombre completo: ")
@@ -7,7 +7,7 @@ ubicacionLocal = input("Introduzca la ubicación de su local: ")
 
 # Imprimir información registrada
 print("¡Registro exitoso!")
-print("Correo electrónico: ",correoElectronico)
+print("Correo electrónico: ",correoElectronicoComercio)
 print("Nombre del comercio: ",nombreComercio)
 print("Teléfono del comercio: ",telefonoComercio)
 print("Nombre del dueño: ",nombreDueno)
